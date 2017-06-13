0f5f05005025474e6f25310dad8377facca7e57c
User <ppkrauss@gmail.com>
2017-06-13 16:10:31 -0300

    update whois e add generator

---
+++ data/domain-whois/_generator.sh
@@ -0,0 +1,18 @@
+mkdir /tmp/whois
+
+whois cienciaaberta.net > /tmp/whois/cienciaaberta.net.txt
+whois cuidando.org.br > /tmp/whois/cuidando.org.br.txt
+whois dialogando.org > /tmp/whois/dialogando.org.txt
+whois escolaDeDados.org > /tmp/whois/escolaDeDados.org.txt
+whois euvoto.org > /tmp/whois/euvoto.org.txt
+whois gastosabertos.org > /tmp/whois/gastosabertos.org.txt
+whois governoaberto.org.br > /tmp/whois/governoaberto.org.br.txt
+whois mira.org.br > /tmp/whois/mira.org.br.txt
+whois mootiro.org > /tmp/whois/mootiro.org.txt
+whois oficial.news > /tmp/whois/oficial.news.txt
+whois ok.org.br > /tmp/whois/ok.org.br.txt
+whois okfn.org.br > /tmp/whois/okfn.org.br.txt
+whois paraOndeFoioMeuDinheiro.org.br > /tmp/whois/paraOndeFoioMeuDinheiro.org.br.txt
+whois queremossaber.org.br > /tmp/whois/queremossaber.org.br.txt
+whois vaimudar.org > /tmp/whois/vaimudar.org.txt
+whois votolegal.org.br > /tmp/whois/votolegal.org.br.txt
